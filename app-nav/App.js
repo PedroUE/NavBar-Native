e0827f3d4de4a38963402a51df9a8ad68dde407d
Pedro Escapalete <pedroescapalete@gmail.com>
2026-04-10 09:58:04 -0300

    ft: finaliza o app

--- app-nav/App.js
+++ app-nav/App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import HomeScreen from './src/HomeScreen';
+import PerfilScreen from './src/PerfilScreen';
+import ConfigScreen from './src/ConfigScreen';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Drawer.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: '#1e40af',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+              drawerActiveTintColor: '#1e40af',
+              drawerActiveBackgroundColor: '#dbeafe',
+              drawerLabelStyle: {
+                fontSize: 16,
+                fontWeight: '500',
+              },
+              drawerStyle: {
+                backgroundColor: '#f8fafc',
+              },
+            }}
+          >
+
+          <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen name="Perfil" component={PerfilScreen} />
+          <Drawer.Screen name="Configurações" component={ConfigScreen} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
